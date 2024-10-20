92d815c0c567c984663fb90a88ca9c54f0494ea4
wgabriel98 <waltergabriel98@gmail.com>
2024-10-20 12:01:21 -0600

    se agregan archivos

---
+++ index.js
@@ -0,0 +1,10 @@
+exports.handler = async (event) => {
+    const number1 = event.number1;
+    const number2 = event.number2;
+    const result = number1 + number2;
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ result: result }),
+    };
+};
